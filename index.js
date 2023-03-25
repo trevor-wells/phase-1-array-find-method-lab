697955292608ea89db30ab1a3892d7c6d5f40179
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-03-25 11:30:54 -0600

    solved with help from solution ONELINE BABY

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function superbowlWin(array){
-    return array.find( element => element.result === "W").year
+    return !!array.find( element => element.result === "W") ? array.find( element => element.result === "W").year : undefined
 }
